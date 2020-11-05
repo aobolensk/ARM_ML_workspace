d5c7c07ce2c2961210406190fe40dc2339584981
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-05 23:44:12 +0300

    Remove -q flag from wget

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ echo "Downloading images..."
 # Download images
 if [ ! -d ./imagenet ]; then
     mkdir imagenet && cd imagenet
-    wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_train_t3.tar -q
+    wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_train_t3.tar
     tar xf ILSVRC2012_img_train_t3.tar
     rm ILSVRC2012_img_train_t3.tar
     tar xf *.tar
@@ -29,7 +29,7 @@ echo "Downloading assets..."
 # Download assets
 if [ ! -d ./alexnet_assets ]; then
     mkdir alexnet_assets && cd alexnet_assets
-    wget https://developer.arm.com//-/media/Arm%20Developer%20Community/Images/Tutorial%20Guide%20Diagrams%20and%20Screenshots/Machine%20Learning/Running%20AlexNet%20on%20Pi%20with%20Compute%20Library/compute_library_alexnet.zip?revision=c1a232fa-f328-451f-9bd6-250b83511e01 -O compute_library_alexnet.zip -q
+    wget https://developer.arm.com//-/media/Arm%20Developer%20Community/Images/Tutorial%20Guide%20Diagrams%20and%20Screenshots/Machine%20Learning/Running%20AlexNet%20on%20Pi%20with%20Compute%20Library/compute_library_alexnet.zip?revision=c1a232fa-f328-451f-9bd6-250b83511e01 -O compute_library_alexnet.zip
     unzip compute_library_alexnet.zip
     rm compute_library_alexnet.zip
 fi
