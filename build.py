db4edc06145c5c75409fdba0543e4023bbc1a575
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-07 02:54:13 +0300

    Small changes for build script

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ def build_compute_library():
     print("Building ComputeLibrary")
     os.chdir("ComputeLibrary")
     os.system('scons Werror=1 debug=0 asserts=0 neon=1 opencl=1 embed_kernels=1 extra_cxx_flags="-fPIC" internal_only=0 arch=arm64-v8a -j`nproc --all`')
-    os.chdir("..")
+    os.chdir(WORKSPACE_DIR)
 
 
 def download_images():
@@ -22,7 +22,6 @@ def download_images():
     print("Downloading images...", flush=True)
     os.mkdir("imagenet")
     os.chdir("imagenet")
-    import wget
     os.system(
         "wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_train_t3.tar")
     print("Unpacking images...")
@@ -31,7 +30,7 @@ def download_images():
     os.system("tar xf *.tar")
     os.system("find -name '*.tar' -exec tar xf {} \;")
     os.system("rm *.tar")
-    os.chdir("..")
+    os.chdir(WORKSPACE_DIR)
 
 
 def download_assets():
@@ -40,13 +39,12 @@ def download_assets():
     print("Downloading assets...")
     os.mkdir("alexnet_assets")
     os.chdir("alexnet_assets")
-    import wget
     os.system(
         "wget https://developer.arm.com//-/media/Arm%20Developer%20Community/Images/Tutorial%20Guide%20Diagrams%20and%20Screenshots/Machine%20Learning/Running%20AlexNet%20on%20Pi%20with%20Compute%20Library/compute_library_alexnet.zip?revision=c1a232fa-f328-451f-9bd6-250b83511e01 -O compute_library_alexnet.zip")
     print("Unpacking assets...")
     os.system("unzip compute_library_alexnet.zip")
     os.system("rm compute_library_alexnet.zip")
-    os.chdir("..")
+    os.chdir(WORKSPACE_DIR)
 
 
 def main():
