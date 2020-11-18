20bfd401319d64faf80bf2ae8264fa86d6f82e45
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-18 21:54:50 +0300

    Replace redirect to /dev/null with -q flag in wget command

--- build.py
+++ build.py
@@ -23,7 +23,7 @@ def download_images():
     os.mkdir("imagenet")
     os.chdir("imagenet")
     os.system(
-        "wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_train_t3.tar &> /dev/null")
+        "wget http://www.image-net.org/challenges/LSVRC/2012/dd31405981ef5f776aa17412e1f0c112/ILSVRC2012_img_train_t3.tar -q")
     print("Unpacking images...")
     os.system("tar xf ILSVRC2012_img_train_t3.tar")
     os.system("rm ILSVRC2012_img_train_t3.tar")
@@ -40,7 +40,7 @@ def download_assets():
     os.mkdir("alexnet_assets")
     os.chdir("alexnet_assets")
     os.system(
-        "wget https://developer.arm.com//-/media/Arm%20Developer%20Community/Images/Tutorial%20Guide%20Diagrams%20and%20Screenshots/Machine%20Learning/Running%20AlexNet%20on%20Pi%20with%20Compute%20Library/compute_library_alexnet.zip?revision=c1a232fa-f328-451f-9bd6-250b83511e01 -O compute_library_alexnet.zip &> /dev/null")
+        "wget https://developer.arm.com//-/media/Arm%20Developer%20Community/Images/Tutorial%20Guide%20Diagrams%20and%20Screenshots/Machine%20Learning/Running%20AlexNet%20on%20Pi%20with%20Compute%20Library/compute_library_alexnet.zip?revision=c1a232fa-f328-451f-9bd6-250b83511e01 -O compute_library_alexnet.zip -q")
     print("Unpacking assets...")
     os.system("unzip compute_library_alexnet.zip")
     os.system("rm compute_library_alexnet.zip")
